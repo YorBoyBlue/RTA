0001c5b530ff25bc7cf9a8c88359c06fbfe3e3eb
YorBoyBlue <arin.a.h.blue@gmail.com>
2017-08-05 10:45:58 -0400

    Camera Follow good

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	
+	public GameObject m_player;
+	public float m_deadSpace;
+	public float m_speed;
+	private bool m_follow;
+
+	public GameObject SetPlayer {set { m_player = value; }}
+
+	void Update() {
+		if(Mathf.Abs(transform.position.x - m_player.transform.position.x) >  m_deadSpace && Mathf.Abs(transform.position.x - m_player.transform.position.x) <  (m_deadSpace + 0.5f) || Mathf.Abs(transform.position.y - m_player.transform.position.y) >  m_deadSpace && Mathf.Abs(transform.position.x - m_player.transform.position.x) <  (m_deadSpace + 0.5f) ) {
+			m_follow = true;			
+		} else if(Mathf.Abs(transform.position.x - m_player.transform.position.x) >  (m_deadSpace + 0.5f) || Mathf.Abs(transform.position.y - m_player.transform.position.y) >  (m_deadSpace + 0.5f)) {
+			//transform.position = new Vector3(m_player.transform.position.x, m_player.transform.position.y, -70);
+			m_follow = true;			
+		} else {
+			m_follow = false;
+		}
+	}
+
+	void FixedUpdate() {
+		if(m_follow) {
+			Vector3 target = new Vector3(m_player.transform.position.x, m_player.transform.position.y, -70);
+			Vector3 currentPos = new Vector3(transform.position.x, transform.position.y, -70);
+			this.transform.position = Vector3.MoveTowards(currentPos, target, (m_speed * Time.fixedDeltaTime));
+		}
+	}
+}
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -2,7 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.Events;
+
+[System.Serializable] public class ToggleEvent : UnityEvent<bool>{}
 
 public class PlayerManager : NetworkBehaviour {
+
+    [SerializeField] ToggleEvent m_toggleLocal;
+    [SerializeField] ToggleEvent m_toggleShared;
+    [SerializeField] ToggleEvent m_toggleRemote;
+
+    bool m_isLocalPlayer = false;
+
+    public bool GetLocalPlayer { get { return m_isLocalPlayer; }}
+
+
 	
+    void Start() {
+        if(isLocalPlayer) {
+            m_isLocalPlayer = true;
+            Camera.main.GetComponent<CameraFollow>().SetPlayer = this.gameObject;
+        } else {
+            Camera.main.gameObject.SetActive(false);
+        }
+    }
 }
