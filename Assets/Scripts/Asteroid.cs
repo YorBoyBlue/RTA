f42063c733df8b57c66bd126de14c5418605e1c2
Tariq Ramyar <tariq@liftlockstudios.com>
2017-08-05 15:52:06 -0400

    Asteroids WIP

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -4,32 +4,45 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour {	
 
-	public const float size_min = .5f;
-	public const float size_decrement = .5f;
+	public const float size_min = 0.5f;
+	public const float size_decrement = 0.5f;
+	public GameObject[] m_Asteroid;
+	private Vector3 velocity;
 
-	public float size;
 
-	public Vector2 direction;
+	public float size;	
 
 	// Use this for initialization
 	void Start () {
-		transform.localScale = Vector3.one * size;
+		//transform.localScale = Vector3.one * size;
+		velocity = new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), 0);
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log("Collision");
 		size -= size_decrement;
 		if (size < size_min) {
 			Destroy(this.gameObject);
+			
 		} else {
 			Split();
 		}
 	}
 
 	void Update() {
-		
+		GetComponent<Rigidbody2D>().AddRelativeForce(velocity);
+		transform.localScale = Vector3.one * size;
 	}
 
 	public void Split() {
+		
+	}
+
+	/// <summary>
+	/// This function is called when the MonoBehaviour will be destroyed.
+	/// </summary>
+	void OnDestroy()
+	{
 		
 	}
 }
--- Assets/Scripts/AsteroidManager.cs
+++ Assets/Scripts/AsteroidManager.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class AsteroidManager : MonoBehaviour {
 
+	public GameObject[] AsteroidPrefabs;
 	// Use this for initialization
 	void Start () {
-		
+		Spawn();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+	public void Spawn(){
+		Instantiate(AsteroidPrefabs[Random.Range(0,1)], this.transform, Quaternion.identity);
+	}
+
+	
 }
