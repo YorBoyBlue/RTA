3507b8421e98ddae62810626a31e8f63056cc16a
NesTHOR <chryslenne.lunarpath@yahoo.com>
2017-08-05 16:47:57 -0400

    [Bullet Update] Added a temporary bullet trails.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,8 +6,24 @@ public class Bullet : MonoBehaviour {
 
 	public Vector2 velocity;
 
+	TrailRenderer tail;
+	Transform thisTransform;
+
 	void Start() {
-		Destroy(this.gameObject, 5f);
+		thisTransform = transform;
+		tail = thisTransform.GetChild(1).GetComponent<TrailRenderer>();
+		Destroy(thisTransform.GetChild(0).gameObject, 5f);
 		GetComponent<Rigidbody2D>().AddRelativeForce(velocity);
+	}
+
+	void Update() {
+		if (thisTransform.childCount == 1) {
+			DestroyBullet();
+		}
+	}
+
+	void DestroyBullet() {
+		GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+		Destroy(gameObject, 1f);
 	}
 }
