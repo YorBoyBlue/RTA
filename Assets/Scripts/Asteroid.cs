8602d0b09ddb7cb16aeb680ee761b37374f90f76
NesTHOR <chryslenne.lunarpath@yahoo.com>
2017-08-06 14:23:23 -0400

    MERGED PROTOTYPE

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -57,12 +57,6 @@ public class Asteroid : NetworkBehaviour {
 				AsteroidManager.singleton.setMedAsteroids(newMedAmount);
 				AsteroidManager.singleton.Spawn(1, other.transform.position);
 				AsteroidManager.singleton.Spawn(1, other.transform.position);
-<<<<<<< HEAD
-=======
-				GetComponent<SpriteRenderer>().color = new Color32(0,0,0,0);
-				GetComponent<ParticleSystem>().Play();
-				Destroy(this.gameObject, 0.4f);				
->>>>>>> 010f65114e2c6a5ebe4debe63edfa9db068ffcf1
 			}
 			if(this.tag == "Medium"){				
 				// int newMedAmount = m_AsteroidManager.getMedAsteroids() - 1;
@@ -73,19 +67,7 @@ public class Asteroid : NetworkBehaviour {
 				int newSmallAmount = AsteroidManager.singleton.getSmallAsteroids() + 2;
 				AsteroidManager.singleton.setSmallAsteroids(newSmallAmount);
 				AsteroidManager.singleton.Spawn(0, other.transform.position);
-<<<<<<< HEAD
 				AsteroidManager.singleton.Spawn(0, other.transform.position);			
-=======
-				AsteroidManager.singleton.Spawn(0, other.transform.position);				
-				Destroy(this.gameObject, 0.4f);
-			}
-			if(this.tag == "Small"){
-				// int newSmallAmount = GetComponentInParent<AsteroidManager>().getSmallAsteroids() - 1;
-				// GetComponentInParent<AsteroidManager>().setSmallAsteroids(newSmallAmount);
-				GetComponent<SpriteRenderer>().color = new Color32(0,0,0,0);
-				GetComponent<ParticleSystem>().Play();
-				Destroy(this.gameObject, 0.4f);
->>>>>>> 010f65114e2c6a5ebe4debe63edfa9db068ffcf1
 			}
 			
 			GetComponent<SpriteRenderer>().color = new Color32(0,0,0,0);
