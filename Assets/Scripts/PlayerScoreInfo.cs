b2584c970e7e00ecdc089fe0af9a36314310dc38
YorBoyBlue <arin.a.h.blue@gmail.com>
2017-08-06 14:39:56 -0400

    Need player back

---
+++ Assets/Scripts/PlayerScoreInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScoreInfo : MonoBehaviour {
+	
+	[SerializeField] Text m_nameText;
+	[SerializeField] Text m_killsText;
+	[SerializeField] Text m_deathsText;
+}
